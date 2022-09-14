803157259497fe1dbcbe390ccacb9650510417df
Arif <kanebrian0634@gmail.com>
2022-09-14 17:38:04 +0300

    todoapp created

---
+++ src/components/Content.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ContentFooter from './ContentFooter';
+import TodoList from './TodoList';
+
+const Content = () => {
+  return (
+    <>
+       <section className="main">
+		<input className="toggle-all" type="checkbox" />
+		<label for="toggle-all">
+			Mark all as complete
+		</label>
+        <TodoList />
+	</section> 
+    <ContentFooter />
+    </>
+    
+  )
+}
+
+export default Content;
