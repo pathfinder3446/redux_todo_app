7e8f96fa0223a59f84ab916a0dd9aaae127dfdf8
Arif <kanebrian0634@gmail.com>
2022-09-14 17:37:21 +0300

    ..

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="info">
+	<p>Click to edit a todo</p>
+	<p>Created by <a href="https://github.com/pathfinder3446">Arif Kılınç</a></p>
+	<p>Part of <a href="">TodoMVC</a></p>
+</footer>
+  )
+}
+
+export default Footer;
