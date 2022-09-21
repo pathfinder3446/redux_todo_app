259a7c0441988d78360704965aeb1bd14d33045a
Arif <kanebrian0634@gmail.com>
2022-09-21 16:37:49 +0300

    ..

--- src/redux/todos/todosSlice.jsx
+++ src/redux/todos/todosSlice.jsx
@@ -1,8 +1,9 @@
 import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const getTodosAsync = createAsyncThunk('todos/getTodosAsync', async() => {
-    const res = await fetch('http://localhost:7000/todos');
-    return await res.json();
+    const res = await axios('http://localhost:7000/todos');
+    return res.data;
 });
 
 export const todosSlice = createSlice({
